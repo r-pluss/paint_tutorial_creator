63dba0fed465c248806e107e6bf1916f8e42ae42
Ryan Pluss <r.pluss52@gmail.com>
2019-04-13 16:36:37 -0400

    still not sure about where the placement of these elements should be

--- js/main.js
+++ js/main.js
@@ -10,8 +10,11 @@ function newRow(){
     let el = document.createElement('div');
     el.classList.add('tut-row');
     let divBtn = document.createElement('div');
+    let unDivBtn = document.createElement('div');
     divBtn.classList.add('divide-row');
+    unDivBtn.classList.add('undivide-row');
     el.appendChild(divBtn);
+    el.appendChild(unDivBtn);
     return el;
 }
 
